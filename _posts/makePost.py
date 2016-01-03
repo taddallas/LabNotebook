d2db2b3370acc7ea607dc23c6571d224f962a38c
taddallas <tdallas@uga.edu>
2016-01-03 15:15:24 -0500

    :eggplant:

--- _posts/makePost.py
+++ _posts/makePost.py
@@ -8,7 +8,7 @@ month = monthDict[today.month]
 day = today.day
 
 newPost = open(fileName+".md", "w")
-newPost.write("---\nlayout: post \ntitle: Status for " + str(month) + " " + str(day) + " \nauthor: Tad\n---\n \n## What did I work on today? \n \n* \n  \n## What should I be working on tomorrow \n* \n \n \n \n--- \n \n### Listening to: \n <iframe src='https://embed.spotify.com/?uri=spotify:track:3bSU0s2VRIP7w82DwJietw' width='300' height='380' frameborder='0' allowtransparency='true'></iframe> \n <i class='fa fa-code' style='color:pink'> </i> ")
+newPost.write("---\nlayout: post \ntitle: Status for " + str(month) + " " + str(day) + " \nauthor: Tad\n---\n \n## What did I work on today? \n \n* \n  \n## What should I be working on tomorrow \n\n* \n \n \n \n--- \n \n### Listening to: \n <iframe src='https://embed.spotify.com/?uri=spotify:track:6XorgdtMRoZ5OdmSV7NCFJ' width='300' height='380' frameborder='0' allowtransparency='true'></iframe> \n <i class='fa fa-code' style='color:pink'></i> ")
 newPost.close()
 
 sys.exit(0) # quit Python
