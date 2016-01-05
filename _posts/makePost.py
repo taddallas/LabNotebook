8363d269685ba650fe0354ff1d02f3df48898478
taddallas <tdallas@uga.edu>
2016-01-05 17:16:19 -0500

    :eggplant:

--- _posts/makePost.py
+++ _posts/makePost.py
@@ -11,4 +11,7 @@ newPost = open(fileName+".md", "w")
 newPost.write("---\nlayout: post \ntitle: Status for " + str(month) + " " + str(day) + " \nauthor: Tad\n---\n \n## What did I work on today? \n \n* \n  \n## What should I be working on tomorrow \n\n* \n \n \n \n--- \n \n### Listening to: \n <iframe src='https://embed.spotify.com/?uri=spotify:track:6XorgdtMRoZ5OdmSV7NCFJ' width='300' height='380' frameborder='0' allowtransparency='true'></iframe> \n <i class='fa fa-code' style='color:pink'></i> ")
 newPost.close()
 
+import webbrowser
+webbrowser.open(fileName+".md", "w")
+
 sys.exit(0) # quit Python
